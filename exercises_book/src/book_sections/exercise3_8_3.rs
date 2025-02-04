4c133561e3489f2ea63253c9c2eb4ee0f5c4a505
JobSiteTest <alejandroivanleyvadiaz@gmail.com>
2025-02-04 16:58:05 -0600

    progress
    progress in exercise

--- exercises_book/src/book_sections/exercise3_8_3.rs
+++ exercises_book/src/book_sections/exercise3_8_3.rs
@@ -5,3 +5,52 @@ user to add employee names to a department in a company; for example,
 retrieve a list of all people in a department or all people in the 
 company by department, sorted alphabetically. (;_;)
 */
+use std::collections::HashMap;
+use std::io;
+pub fn text_interface(){
+	let global_map = HashMap::<&str,&str>::new();
+	println!("Welcome\nChoose an option:");
+	choose_option();
+}
+
+fn choose_option(){
+	println!("1) Add employee to deparment / Update employee deparment");
+	println!("2) List all employees");
+	println!("3) List employees in deparment");
+	println!("4) Exit");
+	let user_string = string_input();
+    match user_string{
+    	val if val == "1".to_string() => add_employee_to_department(),
+    	val if val == "2".to_string() => list_all_alphabetic(),
+    	val if val == "3".to_string() => list_by_department_alphabetic("test".to_string()),
+    	val if val == "4".to_string() => {println!("Exiting");},
+    	String { .. } => {println!("Option not valid");
+    	choose_option();
+    	},
+    }
+}
+
+fn string_input()->String{
+	let mut user_string = String::new();
+    io::stdin()
+    .read_line(&mut user_string)
+    .expect("Failure To read line");
+    let user_string=user_string.trim();
+    user_string.to_string()
+}
+fn add_employee_to_department(){
+	println!("Adding employee");
+}
+
+fn sort_map(){
+
+}
+
+fn list_all_alphabetic(){
+	println!("Listing all employees");
+}
+
+fn list_by_department_alphabetic(deparment_value:String){
+	println!("Listing employees in deparment");
+
+}
--- exercises_book/src/main.rs
+++ exercises_book/src/main.rs
@@ -1,10 +1,12 @@
 mod book_sections;
 
 fn main() {
-    println!("Fibonachi: ");
+    /*println!("Fibonachi: ");
     book_sections::fibonachi_3_5::count_fibonachi();
     println!("Do median and mode: ");
     book_sections::exercise1_8_3::do_median_mode();
     println!("Do pig latin: ");
-    book_sections::exercise2_8_3::test_pig_latin();
+    book_sections::exercise2_8_3::test_pig_latin();*/
+    println!("Do company terminal: ");
+    book_sections::exercise3_8_3::text_interface();
 }
