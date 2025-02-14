2e6920a4de5c8eec375ae46fb3d9a80920d1d43e
JobSiteTest <alejandroivanleyvadiaz@gmail.com>
2025-02-14 16:55:11 -0600

    Test corrected
    Test examples were added
    All test were left to be passed

--- test_example/adder/src/lib.rs
+++ test_example/adder/src/lib.rs
@@ -1,14 +1,138 @@
-pub fn add(left: u64, right: u64) -> u64 {
+pub struct Guess {
+    value: i32,
+}
+// --snip--
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value < 1 {
+            panic!(
+                "Guess value must be greater than or equal to 1, got {value}."
+            );
+        } else if value > 100 {
+            panic!(
+                "Guess value must be less than or equal to 100, got {value}."
+            );
+        }
+
+
+        Guess { value }
+    }
+}
+
+pub fn add(left: i32 , right: i32) -> i32 {
     left + right
 }
+
+pub fn add_two(a: usize) -> usize {
+    a + 2
+}
+pub fn greeting(name: &str) -> String {
+    /*
+    let line = String::from("Hello!")+name;
+    line
+    */
+    let s = format!("Hello! {}", name);
+    s
+}
+
+
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
+
+// this function will fail the test
+/* 
+impl Rectangle {
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width < other.width && self.height > other.height
+    }
+}
+*/
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
+    fn exploration() {
         let result = add(2, 2);
         assert_eq!(result, 4);
+    }
+
+    #[test]
+    fn another() {
+        //panic!("Make this test fail");
+        let result = add(2, -4);
+        assert_eq!(result,-2);
+    }
+
+    #[test]
+    fn larger_can_hold_smaller() {
+        let larger = Rectangle {
+            width: 8,
+            height: 7,
+        };
+        let smaller = Rectangle {
+            width: 5,
+            height: 1,
+        };
+
+        assert!(larger.can_hold(&smaller));
+    }
+
+    #[test]
+    fn smaller_cannot_hold_larger() {
+        let larger = Rectangle {
+            width: 8,
+            height: 7,
+        };
+        let smaller = Rectangle {
+            width: 3,
+            height: 5,
+        };
+
+        assert!(!smaller.can_hold(&larger));
+    }
+
+    #[test]
+    fn it_adds_two() {
+        let result = add_two(2);
+        assert_eq!(result, 4);
+    }
+
+    #[test]
+    fn greeting_contains_name() {
+        let result = greeting("Carol");
+        assert!(
+            result.contains("Carol"),
+            "Greeting did not contain name, value was `{result}`"
+        );
+    }
+
+    //It's enough to pass a substring of the expected panic message
+    #[test]
+    #[should_panic(expected = "less than or equal to 100")]
+    fn greater_than_100() {
+        Guess::new(200);
+    }
+
+    #[test]
+    fn it_works2_generics() -> Result<(), String> {
+        let result = add(2, 2);
+
+        if result == 4 {
+            Ok(())
+        } else {
+            Err(String::from("two plus two does not equal four"))
+        }
     }
 }
